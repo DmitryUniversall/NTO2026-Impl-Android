ac469af34212f5a13912ae7d0a0b35d94aa87bb2
DmitryUniversall <dmitrii.murashov@gmail.com>
2026-03-09 12:34:20 +0300

    Small device UI update

--- app/src/main/java/ru/myitschool/work/ui/screen/main/device/DeviceMainScreen.kt
+++ app/src/main/java/ru/myitschool/work/ui/screen/main/device/DeviceMainScreen.kt
@@ -1,9 +1,17 @@
 package ru.myitschool.work.ui.screen.main.device
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import ru.myitschool.work.R
 
 
 @Composable
@@ -11,5 +19,25 @@ fun DeviceMainScreen(
     navController: NavController,
     viewModel: DeviceMainViewModel = viewModel()
 ) {
-    Text(text = "TEST")
+    val colors = MaterialTheme.colorScheme
+    val typography = MaterialTheme.typography
+
+    Column() {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(32.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "15:00",
+                style = typography.displayMedium,
+                color = colors.onBackground
+            )
+
+            Text(
+                text = "Пятница, 3 марта 2026",
+                style = typography.headlineSmall,
+                color = colors.tertiary
+            )
+        }
+    }
 }
